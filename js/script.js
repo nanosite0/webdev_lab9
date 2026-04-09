7a5ce984942d507b5e51c0327e8d1c9bab60cd18
nanosite0 <lmonto@proton.me>
2026-04-08 23:10:40 -0500

    Update h2 value
    H2 text value gets updated to text entry after click

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@ function init(){
 
   const enter = document.getElementById("entrybutton");
   var entry = document.getElementById("entryinput");
+  var h2 = document.getElementById("textoutput");
   
   enter.addEventListener("click", entryClick);
   
   function entryClick(){
+    h2.textContent = entry.value;
     alert("Leonardo: " + entry.value);
   }
 }
