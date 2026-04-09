9db9683487b476e0bbcb315d2d703301d4382fd9
nanosite0 <lmonto@proton.me>
2026-04-08 22:49:05 -0500

    Update script.js
    Tested click function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 function init(){
 //add your javascript between these two lines of code
- 
-
-
-
+  console.log("Test");
+  var entry = document.getElementById("entrybutton");
+  
+  entry.addEventListener("click", entryClick);
+  
+  function entryClick(){
+  alert("message");
+  }
+}
 
 window.addEventListener('load', init);
